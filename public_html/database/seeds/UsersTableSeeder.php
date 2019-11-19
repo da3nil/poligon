87983a4f5df6f7ca669d0e68b047b7c9b660db8e
danil <d.prytckov@yandex.ru>
2019-11-19 13:58:48 +0500

    Add factory and update seeders

--- public_html/database/seeds/UsersTableSeeder.php
+++ public_html/database/seeds/UsersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +13,19 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data = [
+            [
+                'name'      => 'Автор не известен',
+                'email'     => 'author_unknow@g.g',
+                'password'  => bcrypt(Str::random(16))
+            ],
+            [
+                'name'      => 'Автор',
+                'email'     => 'author1@g.g',
+                'password'  => bcrypt('123123')
+            ]
+        ];
+
+        DB::table('users')->insert($data);
     }
 }
