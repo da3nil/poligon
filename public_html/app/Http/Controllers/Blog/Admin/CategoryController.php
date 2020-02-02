f228ba2a5a43fef3ef7d0b33e840675c884d7075
danil <d.prytckov@yandex.ru>
2020-02-02 09:37:07 +0500

    ADD store method in CategoryController.php

--- public_html/app/Http/Controllers/Blog/Admin/CategoryController.php
+++ public_html/app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -39,7 +39,7 @@ class CategoryController extends BaseController
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function store(BlogCategoryCreateRequest $request)
     {
